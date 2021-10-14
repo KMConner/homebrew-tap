496382040a3ba8e516b2cd0948f99d455354d840
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-14 13:11:35 +0900

    Brew formula update for kubectl-auth0 version v0.2.0

--- Formula/kubectl-auth0.rb
+++ Formula/kubectl-auth0.rb
@@ -5,20 +5,20 @@
 class KubectlAuth0 < Formula
   desc "Sign in k8s cluster using auth0"
   homepage "https://github.com/KMConner/kubectl-auth0"
-  version "0.1.1"
+  version "0.2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/KMConner/kubectl-auth0/releases/download/v0.1.1/kubectl-auth0_0.1.1_Darwin_x86_64.tar.gz"
-      sha256 "75e4ce74fd83f6a839046eada863e5d5450f3f20ead3650d6a947bbeb180d949"
+      url "https://github.com/KMConner/kubectl-auth0/releases/download/v0.2.0/kubectl-auth0_0.2.0_Darwin_x86_64.tar.gz"
+      sha256 "6865f81a08896cbb2f560063e4edfa9703b880be034fa5404c4d4dc83fca0a1f"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/KMConner/kubectl-auth0/releases/download/v0.1.1/kubectl-auth0_0.1.1_Linux_x86_64.tar.gz"
-      sha256 "b5bbae004fdbfc5153b5b4fc76963a1e864ca37cfe57de72fb0a606f22d310bb"
+      url "https://github.com/KMConner/kubectl-auth0/releases/download/v0.2.0/kubectl-auth0_0.2.0_Linux_x86_64.tar.gz"
+      sha256 "056b3fa9ad63ba8da8781d77554a0e526d71c85cba98e326ee963464d7694a6a"
     end
   end
 
